449635890310eb0e15b3fa6ca840d896ff542d15
Abdul Aziz <forabdulaziz07@gmail.com>
2026-03-02 02:13:59 +0530

    03_pattern

---
+++ pattern/03_pattern.py
@@ -0,0 +1,25 @@
+#Given an integer n. You need to recreate the pattern given below for any value of N.
+#  Let's say for N = 5, the pattern should look like as below:
+
+# *
+# **
+# ***
+# ****
+# *****
+
+
+
+# Take input from user and convert it to integer
+num = int(input("enter a num: "))
+
+# Outer loop → controls number of rows
+# It runs from 1 to num +1
+for i in range(1, num + 1):
+    
+    # Inner loop → prints numbers from 1 to current row number (i)
+    for j in range(1, i + 1):
+        
+        # Print number without moving to next line
+     print(j, end="")
+    # After inner loop finishes, move to next line
+    print()
