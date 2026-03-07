8247cb23521a27a3dec76c1ffe4a230781e7cef1
Abdul Aziz <forabdulaziz07@gmail.com>
2026-03-07 21:19:37 +0530

    add comment

--- 07_pattern.py
+++ 07_pattern.py
@@ -1,4 +1,18 @@
+# Given an integer n. You need to recreate the pattern given below for any value of N.
+#  Let's say for N = 5, the pattern should look like as below:
+
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+
+#first get usre input uing input and convert using int()
 n = int(input("Enter n: "))
 
+#create a loop starts from 1 and till n +1 
 for i in range(1, n + 1):
-    print(" " * (n - i) + "*" * (2 * i - 1))
+# print space by multiplying the empty string ,for example user input =5 , 5-i = 4 so empty string * 4 ="    " 
+# print    "*" * (2*1-1)= 1 so it will print 1* "*"= *
+# then using + concantation we add spaces and "* together"
+    print(" " * (n - i)  +  "*" * (2 * i - 1))
