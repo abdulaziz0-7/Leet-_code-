b179f4b276efb892b6640812b364eb4d0b88fd53
abdulaziz07 <forabdulaziz07@gmail.com>
2026-03-14 23:55:38 +0530

    14_patter

--- pattern/14_pattern.py
+++ pattern/14_pattern.py
@@ -1,13 +1,15 @@
-# Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
-
-
+# Given an integer n. You need to recreate the pattern given below for any value of N. 
+#  Let's say for N = 5, the pattern should look like as below:
 
 # A
-
 # AB
-
 # ABC
-
 # ABCD
-
 # ABCDE
+
+n = int(input("Enter n: "))
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print(chr(65 + j), end="")
+    print()
