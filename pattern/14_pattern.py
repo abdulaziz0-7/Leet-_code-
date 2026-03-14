52308a44f3cdc0af909644774d1077c9afb691ec
abdulaziz07 <forabdulaziz07@gmail.com>
2026-03-14 23:58:07 +0530

    add comments

--- pattern/14_pattern.py
+++ pattern/14_pattern.py
@@ -7,9 +7,12 @@
 # ABCD
 # ABCDE
 
+#gettimg input from user
 n = int(input("Enter n: "))
 
+#creating a for loop star from 1 , stops n+1
 for i in range(1, n + 1):
+    
     for j in range(i):
         print(chr(65 + j), end="")
     print()
