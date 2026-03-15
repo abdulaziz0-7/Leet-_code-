5fc4e08487b4d57625fbd17c813c5cf0e97e45eb
abdulaziz07 <forabdulaziz07@gmail.com>
2026-03-15 23:36:40 +0530

    commmit

--- pattern/15_patter.py
+++ pattern/15_patter.py
@@ -1,6 +1,6 @@
 n = int(input("Enter n: "))
 
-#creating a for loop star from 1 , stops n+1
+#creating a for loop star from n , stops at 0 and ad step as -1
 for i in range(n, 0 , -1):
     
     for j in range(i):
