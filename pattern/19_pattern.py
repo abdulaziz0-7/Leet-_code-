d24a1465385b9f5a6d9f3d92c0c27ad6a8ee411c
abdulaziz07 <forabdulaziz07@gmail.com>
2026-03-16 23:32:59 +0530

    19_pattern

---
+++ pattern/19_pattern.py
@@ -0,0 +1,26 @@
+# Given an integer n. You need to recreate the pattern given below for any value of N. 
+# Let's say for N = 5, the pattern should look like as below:
+
+
+# **********
+# ****  ****
+# ***    ***
+# **      **
+# *        *
+# *        *
+# **      **
+# ***    ***
+# ****  ****
+# **********
+
+# we get the value of n from the user and store it in a variable n    
+n= int(input("enter a number:"))
+
+# creating a for loop to print first half of the pattern by  setting the range from n to 0 and decrements by -1
+for i in range(n,0,-1):
+# first we print "*" multiplying it with i then adding space by multiplying " " with n-1 thena again multiplying " " with n-1 and adding "*" multiplyed wit i   
+    print("*"*i +" "*(n-i)+" "*(n-i)+"*"*i) 
+# creating a for loop to print second  half of the pattern by  setting the range from 1 and ends at n increments by 1
+for i in range(1,n+1,1):
+    
+    print("*"*i+" "*(n-i)+" "*(n-i)+"*"*i)     
